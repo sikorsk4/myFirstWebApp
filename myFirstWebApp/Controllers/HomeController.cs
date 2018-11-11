fee7ad5330f6688cdab71fff7f41bf992b357d88
jmsikorski <sikorsk4@gmail.com>
2018-11-11 07:35:13 -0800

    Added basic contact form to Contact.cshtml

--- myFirstWebApp/Controllers/HomeController.cs
+++ myFirstWebApp/Controllers/HomeController.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using myFirstWebApp.Models;
+
 
 namespace myFirstWebApp.Controllers
 {
@@ -29,6 +34,13 @@ namespace myFirstWebApp.Controllers
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Contact(string name, string message, string subject)
+        {
+            SendContactEmail(name, message, subject);
+            return Content("Thanks for contacting us! We'll be in contact with you soon!");
+        }
+
         public IActionResult Privacy()
         {
             return View();
@@ -38,6 +50,37 @@ namespace myFirstWebApp.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public void SendContactEmail(string user, string message, string subject)
+        {
+            // Your hard-coded email values (where the email will be sent from), this could be
+            // define in a config file, etc.
+            var email = "sikorsk4@gmail.com";
+            var password = "{Password}"; 
+
+            // Your target (you may want to ensure that you have a property within your form so that you know
+            // who to send the email to
+            string address = "dicedlr99@gmail.com";
+
+            // Builds a message and necessary credentials (example using Gmail)
+            var loginInfo = new NetworkCredential(email, password);
+            var msg = new MailMessage();
+            var smtpClient = new SmtpClient("smtp.gmail.com", 587);
+
+            // This email will be sent from you
+            // msg.From = new MailAddress(email);
+            msg.From = new MailAddress(user);
+            // Your target email address
+            msg.To.Add(new MailAddress(address));
+            msg.Subject = subject + " from " + user;
+            // Build the body of your email using the Body property of your message
+            msg.Body = message;
+
+            // Wires up and send the email
+            smtpClient.EnableSsl = true;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = loginInfo;
+            smtpClient.Send(msg);
         }
     }
 }
